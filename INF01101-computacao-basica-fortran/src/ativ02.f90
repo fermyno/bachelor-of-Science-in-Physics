eaba860c9fbdae6b69bc7697b014ead0b267b032
Fermyno Gutierrez <fermyno@gmail.com>
2023-01-04 13:13:35 +0000

    source code

---
+++ INF01101-computacao-basica-fortran/src/ativ02.f90
@@ -0,0 +1,13 @@
+! Enunciado: Faça um programa que leia um número do teclado e escreva na tela
+
+program ativ02
+implicit none
+real :: a
+
+print *, "PROGRAMA QUE LE UM NUMERO DO TECLADO E ESCREVE NA TELA"
+
+print *, "Informe o valor de a (numero): "
+read *, a
+print *, " Valor lido : ",a
+
+end program ativ02
