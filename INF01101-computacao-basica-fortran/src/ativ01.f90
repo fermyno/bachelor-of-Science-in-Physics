bf94aa2675d947d2c6017220faae6dfca5d0bcc7
Fermyno Gutierrez <fermyno@gmail.com>
2023-01-04 13:13:15 +0000

    source code

---
+++ INF01101-computacao-basica-fortran/src/ativ01.f90
@@ -0,0 +1,8 @@
+! Enunciado: Faça um programa que escreva uma mensagem na tela
+
+program ativ01
+implicit none
+print *, "PROGRAMA QUE ESCREVE MENSAGEM NA TELA"
+print *, ""
+print *, "Ola Mundo da Fisica!"
+end program ativ01
